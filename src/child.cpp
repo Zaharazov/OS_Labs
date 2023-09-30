e90415d759fbb3ff655b452b4569c8cef67b0809
Захар <104322009+Zaharazov@users.noreply.github.com>
2023-10-01 00:31:14 +0300

    Create child.cpp

---
+++ src/child.cpp
@@ -0,0 +1,32 @@
+#include <fcntl.h> // for open 
+#include <unistd.h> // for read() and write()
+#include <string>
+
+int main()
+{
+    int output;
+    output = open("output.txt", O_WRONLY, 0);
+    std::string parentdata;
+    float divisible = 0;
+
+    while (read(STDIN_FILENO, &parentdata, sizeof(parentdata)) != 0) {
+        if (divisible == 0)
+        {
+            divisible = std::stof(parentdata);
+        }
+        else
+        {
+            if (std::stof(parentdata) == 0)   exit(-1);
+
+            divisible /= std::stof(parentdata);
+        }
+    }
+
+    std::string div_str = std::to_string(divisible);
+    char answer[100];
+    sprintf(answer, "The answer is: %f", divisible);
+
+    if (write(output, &answer, 12 + div_str.length()) == -1) {
+        return -1;
+    }
+}
